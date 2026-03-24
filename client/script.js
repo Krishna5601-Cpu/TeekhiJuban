7f0b22f5dd6ea3216fd5bb4091a405428fa364be
Krishna <krishnachauhan2724@gmail.com>
2026-03-24 21:52:00 +0530

    feat(flow): implemented landing to roast flow state transition using vanilla JS controller(DOM Selectors)

--- client/script.js
+++ client/script.js
@@ -1,3 +1,4 @@
+// State Management -
 let state = "home";
 
 function renderView() {
@@ -8,3 +9,23 @@ function renderView() {
   if (state === "result") {
   }
 }
+
+// DOM Selectors -
+
+const startBtn = document.querySelector("#startRoast");
+const appFlow = document.querySelector("#app-flow");
+
+const hero = document.querySelector(".hero");
+const how = document.querySelector(".how");
+const sample = document.querySelector(".sample");
+const cta = document.querySelector(".cta");
+
+// Event Listner -
+startBtn.addEventListener("click", () => {
+  hero.style.display = "none";
+  how.style.display = "none";
+  sample.style.display = "none";
+  cta.style.display = "none";
+
+  appFlow.style.display = "block";
+});
